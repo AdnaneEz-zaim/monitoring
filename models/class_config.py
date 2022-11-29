e9d4201bc73dc6757ea790074c2a00a2a649e5de
adnane ez-zaim <ad.ezzaim@gmail.com>
2022-11-29 11:39:34 +0100

    class connexion and config

--- models/class_config.py
+++ models/class_config.py
@@ -1,17 +1,37 @@
+import configparser
 class Config:
 
-    def __init__(self,hostnames,usernames,passwords,ports):
-        self.hostnames = hostnames
-        self.usernames = usernames
-        self.passwords = passwords
-        self.ports = ports
+    def __init__(self,id):
+        # CONFIGURATION FILE PATH
+        path_configFile = "config.ini"
+
+        # Loading configuration file ----------------------------------------
+        config = configparser.ConfigParser()
+        config.read(path_configFile)
+
+        machines_hostnames = config['monitored_machines']['hostnames'].split(';')
+        machines_usernames = config['monitored_machines']['usernames'].split(';')
+        machines_passwords = config['monitored_machines']['passwords'].split(';')
+        machines_ports = config['monitored_machines']['ports'].split(';')
+
+        print(machines_hostnames[id])
+        print(machines_usernames[id])
+        print(machines_passwords[id])
+        print(machines_ports[id])
+        print("Configuration succesfully loaded !")
+
+        self.hostname = machines_hostnames[id]
+        self.username = machines_usernames[id]
+        self.password = machines_passwords[id]
+        self.port = machines_ports[id]
         
     # getter method
-    def get_hostnames(self):
-        return self.hostnames
-    def get_usernames(self):
-        return self.usernames
-    def get_passwords(self):
-        return self.passwords
-    def get_ports(self):
-        return self.ports
+    def get_hostname(self):
+        return self.hostname
+    def get_username(self):
+        return self.username
+    def get_password(self):
+        return self.password
+    def get_port(self):
+        return self.port
+
--- models/class_connexion.py
+++ models/class_connexion.py
@@ -3,22 +3,11 @@ import paramiko
 class Connexion :
 
     def __init__(self,monitor):
-       self.monitor=monitor
-
-    
-    def connect():
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-        client.connect(hostname=monitor0.get_hostnames, username=monitor0.get_usernames, password=monitor0.get_passwords, port=monitor0.get_ports)
-        return client
-
-    # getter method
-    def get_hostnames(self):
-        return self.hostnames
-    def get_usernames(self):
-        return self.usernames
-    def get_passwords(self):
-        return self.passwords
-    def get_ports(self):
-        return self.ports
+        self.monitor=monitor
+        self.client = paramiko.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        self.client.connect(hostname=self.monitor.hostname, username=self.monitor.username, password=self.monitor.password, port=self.monitor.port)
+        
+    def getClient():
+        return self.client
