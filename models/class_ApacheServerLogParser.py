2eb9219e72aa29b362b09f2298dd3bf6414667b9
paillard.nicolas <nicolaspaillard721@yahoo.fr>
2022-12-13 14:34:22 +0100

    Pylint Parser files updated

--- models/class_ApacheServerLogParser.py
+++ models/class_ApacheServerLogParser.py
@@ -1,13 +1,15 @@
-import apache_log_parser
+"""Class to parse the log files we use"""
 from pprint import pprint
+import apache_log_parser
 
 
 class LogParser:
-
+    """ Class that parses the info from the log files we use"""
     def __init__(self, raw_line):
         self.line_parser = apache_log_parser.make_parser("%h %a %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"")
         self.log_line_data = self.line_parser(raw_line)
         #pprint(self.log_line_data)
 
-    def getLogLineData(self):
+    def get_log_line_data(self):
+        """method to get a line from the datas"""
         return self.log_line_data
