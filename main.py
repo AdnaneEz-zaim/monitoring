820f4318851dffbca5c931f92a705178fe080ecc
axelantoine <ax.antoine42@gmail.com>
2023-01-11 17:56:51 +0100

    Adding auto refresh with callback on all figure

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 import threading
 import time
 import dash
+from dash.dependencies import Input, Output
 
 from models.ConfigurationLoader import Config
 from models.Connexion import Connexion
@@ -19,24 +20,82 @@ monitors = []
 connexions = []
 clients = []
 logInfos = []
-
+csv_fileNames = [[] for _ in range(nbMachineConfiguration)]
+uptime_serverResults = [None] * nbMachineConfiguration
+cpuModel_server = [None] * nbMachineConfiguration
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "GROUP 6 Server Monitoring Dashboard"
 
 # Init
-for m in range(nbMachineConfiguration):
-    monitors.append(machineConfiguration.loadMachineConfiguration(m))
-    connexions.append(Connexion(monitors[m]))
-    clients.append(connexions[m].client)
+for h in range(nbMachineConfiguration):
+    monitors.append(machineConfiguration.loadMachineConfiguration(h))
+    connexions.append(Connexion(monitors[h]))
+    clients.append(connexions[h].client)
     logInfos.append(LogInfo())
+
+    # CREATE CSV NAMES
+    # APACHE LOG -----------------------------------------------------
+    # CSV Error code
+    csv_filename = machineConfiguration.machines_hostnames[h] \
+                   + '_apacheLog_statusCode404' \
+                   + '.csv'
+    csv_fileNames[h].append(csv_filename)
+
+    # CSV Client connect
+    csv_filename = machineConfiguration.machines_hostnames[h] \
+                   + '_apacheLog_clientConnect' \
+                   + '.csv'
+    csv_fileNames[h].append(csv_filename)
+
+    # CSV URL request
+    csv_filename = machineConfiguration.machines_hostnames[h] \
+                   + '_apacheLog_requestUrl' \
+                   + '.csv'
+    csv_fileNames[h].append(csv_filename)
+
+    # Hardware usage ---------------------------------------
+    csv_filename = machineConfiguration.machines_hostnames[h] \
+                   + '_hardwareUsage' \
+                   + '.csv'
+    csv_fileNames[h].append(csv_filename)
 
 logging.basicConfig(level=logging.DEBUG,
                     filename="monitors.log",
                     format='%(message)s'
                     )
 
+def createAppLayout(list_csv):
+    list_layout = []
+
+    list_layout.append(Dashboard.generate_header_layout())
+    # For every monitored machines
+    for host_id in range(nbMachineConfiguration):
+        # Get all csv file name that start with the currrent machine hostname
+        csv_hostname = machineConfiguration.machines_hostnames[host_id]
+
+        # Get csv of current hostname
+        filtered_csv_list = [s for s in list_csv[host_id] if s.startswith(csv_hostname)]
+        # Get hardware csv of current hostname
+        hardware_csv_list = [csvName for csvName in filtered_csv_list if "hardware" in csvName]
+        # Get apache csv of current hostname
+        apache_csv_list = [csvName for csvName in filtered_csv_list if "apache" in csvName]
+
+        # Generate hostname title layout
+        list_layout.append(Dashboard.generate_hostname_title(csv_hostname, "NULL", host_id))
+
+        # Generate layout for hardware usage
+        list_layout.append(Dashboard.generate_layout_hardware(hardware_csv_list, "NULL", host_id))
+
+        # Generate layout for apache log
+        list_layout.append(Dashboard.generate_layout_apache(apache_csv_list, host_id))
+
+    list_layout.append(Dashboard.generate_interval_component())
+
+    app.layout = Dashboard.generate_app_layout(list_layout)
+
+    print("DASH LAYOUT CREATED")
 
 def get_data():
     while True:
@@ -51,8 +110,8 @@ def get_data():
         apache_statusCode_results = []
         apache_clientConnect_results = []
         apache_requestUrl_results = []
-        uptime_serverResults = []
-        cpuModel_server = []
+        uptime_serverResults.clear()
+        cpuModel_server.clear()
 
         # Get result from thread when they have finish their tasks
         for t in MonitorThreads:
@@ -72,80 +131,96 @@ def get_data():
             apache_clientConnect_results.append(t.apache_clientConnect_result)
             apache_requestUrl_results.append(t.apache_requestUrl_result)
 
-        print("DATA SCRAPPING DONE")
+        print("-[BACKEND]-DATA SCRAPPING DONE")
 
         # CSV Filling
-        csv_fileNames = []
         for host_id in range(nbMachineConfiguration):
-            # APACHE LOG -----------------------------------------------------
-            # CSV Error code
-            csv_filename = machineConfiguration.machines_hostnames[host_id] \
-                           + '_apacheLog_statusCode404' \
-                           + '.csv'
-            csv_fileNames.append(csv_filename)
 
-            fill_csv(csv_filename, apache_statusCode_results, host_id)
+            fill_csv(csv_fileNames[host_id][0], apache_statusCode_results, host_id)
+            fill_csv(csv_fileNames[host_id][1], apache_clientConnect_results, host_id)
+            fill_csv(csv_fileNames[host_id][2], apache_requestUrl_results, host_id)
+            fill_csv(csv_fileNames[host_id][3], hardwareUsageResults, host_id)
 
-            # CSV Client connect
-            csv_filename = machineConfiguration.machines_hostnames[host_id] \
-                           + '_apacheLog_clientConnect' \
-                           + '.csv'
-            csv_fileNames.append(csv_filename)
+        print("-[BACKEND]-CSV FILLING DONE")
 
-            fill_csv(csv_filename, apache_clientConnect_results, host_id)
-
-            # CSV URL request
-            csv_filename = machineConfiguration.machines_hostnames[host_id] \
-                           + '_apacheLog_requestUrl' \
-                           + '.csv'
-            csv_fileNames.append(csv_filename)
-
-            fill_csv(csv_filename, apache_requestUrl_results, host_id)
-
-            # Hardware usage ---------------------------------------
-            csv_filename = machineConfiguration.machines_hostnames[host_id] + '_hardwareUsage' + '.csv'
-            csv_fileNames.append(csv_filename)
-
-            fill_csv(csv_filename, hardwareUsageResults, host_id)
-
-        print("CSV FILLING DONE")
-
-        update_server_metrics(csv_fileNames, uptime_serverResults, cpuModel_server)
         time.sleep(5)
 
 
-def update_server_metrics(list_csv, uptime_serverResults, cpuModel_server):
-    list_layout = [Dashboard.generate_header_layout()]
+@app.callback(
+        [
+        Output('cpu-usage-graph0', 'figure'),
+        Output('mem-usage-graph0', 'figure'),
+        Output('sto-usage-graph0', 'figure'),
+        Output('cpu-usage-graph1', 'figure'),
+        Output('mem-usage-graph1', 'figure'),
+        Output('sto-usage-graph1', 'figure')
+        ],
+        [Input('interval-component', 'n_intervals')])
+def update_graph_cpu_usage(n_intervals):
+    print("-[UI]-UPDATE HARDWARE USAGE GRAPH")
+    figures = []
+    for host_id in range(nbMachineConfiguration):
+        update_figures = Dashboard.update_hardware_usage(csv_fileNames[host_id][3], cpuModel_server[host_id][0])
+        for fig in update_figures:
+            figures.append(fig)
 
-    # For every monitored machines
+    return figures
+
+@app.callback(
+    [
+        Output('error-code-graph0', 'figure'),
+        Output('client-connect-graph0', 'figure'),
+        Output('requestUrl-graph0', 'figure'),
+        Output('error-code-graph1', 'figure'),
+        Output('client-connect-graph1', 'figure'),
+        Output('requestUrl-graph1', 'figure')
+    ],
+    [
+        Input('interval-component','n_intervals')
+    ]
+)
+def update_graph_apache(n_intervals):
+    print("-[UI]-UPDATE APACHE LOG GRAPH")
+    figures = []
     for host_id in range(nbMachineConfiguration):
 
-        # Get all csv file name that start with the currrent machine hostname
         csv_hostname = machineConfiguration.machines_hostnames[host_id]
-        
+
         # Get csv of current hostname
-        filtered_csv_list = [s for s in list_csv if s.startswith(csv_hostname)]
-        # Get hardware csv of current hostname
-        hardware_csv_list = [csvName for csvName in filtered_csv_list if "hardware" in csvName]
+        filtered_csv_list = [s for s in csv_fileNames[host_id] if s.startswith(csv_hostname)]
+
         # Get apache csv of current hostname
         apache_csv_list = [csvName for csvName in filtered_csv_list if "apache" in csvName]
-        
-        # Generate hostname title layout
-        list_layout.append(Dashboard.generate_hostname_title(csv_hostname, uptime_serverResults, host_id))
 
-        # Generate layout for hardware usage
-        list_layout.append(Dashboard.generate_layout_hardware(hardware_csv_list, cpuModel_server, host_id))
+        update_figures = Dashboard.update_apache_log(apache_csv_list, host_id)
+        for fig in update_figures:
+            figures.append(fig)
 
-        # Generate layout for apache log
-        list_layout.append(Dashboard.generate_layout_apache(apache_csv_list, host_id))
+    return figures
 
-    app.layout = Dashboard.generate_app_layout(list_layout)
 
-    print("DASH UPDATED")
+@app.callback(
+    [
+        Output('uptime0', 'children'),
+        Output('uptime1', 'children')
+    ],
+    [
+        Input('interval-component','n_intervals')
+    ]
+)
+def update_uptime(n_intervals):
+    print("-[UI]-UPDATE UPTIME ---")
+    childrens = []
+    for host_id in range(nbMachineConfiguration):
+        childrens.append(Dashboard.update_uptime(uptime_serverResults[host_id][0]))
+    return childrens
 
 
 # Init the data scrapper thread
 dataScrapperThread = threading.Thread(target=get_data)
 # Start the data scrapper thread
 dataScrapperThread.start()
-app.run_server()
+
+createAppLayout(csv_fileNames)
+app.run_server(debug=True)
+
--- models/Dash.py
+++ models/Dash.py
@@ -8,16 +8,28 @@ def generate_header_layout():
                 className="header-title",
                 )
 
-        ],
+    ],
         className="header"
     )
     return header_layout
 
 
+def generate_interval_component():
+    interval_layout = html.Div([
+        dcc.Interval(
+            id='interval-component',
+            interval=10000,  # en ms
+            n_intervals=0
+        )
+    ])
+    return interval_layout
+
+
 def generate_hostname_title(hostname, uptime_serverResults, host_id):
+    uptime_id = "uptime" + str(host_id)
     hostname_layout = html.Div(children=[
         html.H2(children=hostname, className="hostname"),
-        html.H6(children="Uptime : " + str(uptime_serverResults[host_id][0]), className="uptime")
+        html.H6(children="Uptime : " + str(uptime_serverResults[host_id][0]), className="uptime", id=uptime_id)
     ])
     return hostname_layout
 
@@ -25,16 +37,19 @@ def generate_hostname_title(hostname, uptime_serverResults, host_id):
 def generate_layout_hardware(hardware_csv_list, cpuModel_server, host_id):
     # HARDWARE USAGE FRAME
     for csv_name in hardware_csv_list:
-
         df = pd.read_csv(csv_name)
 
         # Create layout hardware usage
         cpuModel = str(cpuModel_server[host_id][0])
+        graph_cpu_id = "cpu-usage-graph" + str(host_id)
+        graph_mem_id = "mem-usage-graph" + str(host_id)
+        graph_sto_id = "sto-usage-graph" + str(host_id)
+
         hardware_layout = \
             html.Div(
                 children=[
                     dcc.Graph(
-                        id="cpu-usage-graph",
+                        id=graph_cpu_id,
                         figure={
                             "data": [
                                 {
@@ -61,7 +76,7 @@ def generate_layout_hardware(hardware_csv_list, cpuModel_server, host_id):
                         },
                     ),
                     dcc.Graph(
-                        id="mem-usage-graph",
+                        id=graph_mem_id,
                         figure={
                             "data": [
                                 {
@@ -88,7 +103,7 @@ def generate_layout_hardware(hardware_csv_list, cpuModel_server, host_id):
                         },
                     ),
                     dcc.Graph(
-                        id="sto-usage-graph",
+                        id=graph_sto_id,
                         figure={
                             "data": [
                                 {
@@ -120,20 +135,23 @@ def generate_layout_hardware(hardware_csv_list, cpuModel_server, host_id):
         return hardware_layout
     return html.Div(children=[])
 
-def generate_layout_apache(list_csvname, host_id):
 
+def generate_layout_apache(list_csvname, host_id):
     df_list = []
     for csvApache in list_csvname:
         df_list.append(pd.read_csv(csvApache))
 
-        # Create layout apache log
+    # Create layout apache log
+    graph_error_id = "error-code-graph" + str(host_id)
+    graph_clientConnect_id = "client-connect-graph" + str(host_id)
+    graph_resultURL_id = "requestUrl-graph" + str(host_id)
 
     if len(df_list) == 3:
         apache_layout = \
             html.Div(
                 children=[
                     dcc.Graph(
-                        id="error-code-graph",
+                        id=graph_error_id,
                         figure={
                             "data": [
                                 {
@@ -157,7 +175,7 @@ def generate_layout_apache(list_csvname, host_id):
                         },
                     ),
                     dcc.Graph(
-                        id="client-connect-graph",
+                        id=graph_clientConnect_id,
                         figure={
                             "data": [
                                 {
@@ -182,7 +200,7 @@ def generate_layout_apache(list_csvname, host_id):
                         },
                     ),
                     dcc.Graph(
-                        id="sto-usage-graph",
+                        id=graph_resultURL_id,
                         figure={
                             "data": [
                                 {
@@ -216,3 +234,180 @@ def generate_layout_apache(list_csvname, host_id):
 
 def generate_app_layout(list_layout):
     return html.Div(children=list_layout)
+
+
+def update_hardware_usage(csvName, cpuModel_server):
+    figures = []
+    df = pd.read_csv(csvName)
+    # Create layout hardware usage
+    cpuModel = str(cpuModel_server)
+
+    fig_cpu = {
+        "data": [
+            {
+                "x": df["Date"],
+                "y": df["CPU_USAGE"],
+                "type": "lines",
+                "hovertemplate": "%{y:.2f}% %{x}"
+                                 "<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "CPU usage [" + cpuModel + "]",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": False},
+            "yaxis": {
+                "ticksuffix": "%",
+                "fixedrange": False,
+            },
+            "colorway": ["#17B897"],
+        },
+    }
+
+    figures.append(fig_cpu)
+
+    fig_mem = {
+        "data": [
+            {
+                "x": df["Date"],
+                "y": df["MEM_USAGE"],
+                "type": "lines",
+                "hovertemplate": "%{y:.2f}% %{x}"
+                                 "<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Memory usage",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": False},
+            "yaxis": {
+                "ticksuffix": "%",
+                "fixedrange": False,
+            },
+            "colorway": ["#9b59b6"],
+        },
+    }
+
+    figures.append(fig_mem)
+
+    fig_sto = {
+        "data": [
+            {
+                "x": df["Date"],
+                "y": df["STO_USAGE"],
+                "type": "lines",
+                "hovertemplate": "%{y:.2f}% %{x}"
+                                 "<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Storage usage",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": False},
+            "yaxis": {
+                "ticksuffix": "%",
+                "fixedrange": False,
+            },
+            "colorway": ["#3498db"],
+        },
+    }
+
+    figures.append(fig_sto)
+
+    return figures
+
+def update_apache_log(list_csvname, host_id):
+
+    figures = []
+
+    df_list = []
+    for csvApache in list_csvname:
+        df_list.append(pd.read_csv(csvApache))
+
+    fig_error = {
+        "data": [
+         {
+             "x": df_list[0]["Date"],
+             "y": df_list[0]["OCCURRENCE"],
+             "type": "bar",
+             "hovertemplate": "%{y} %{x}"
+                              "<extra></extra>",
+         },
+        ],
+        "layout": {
+         "title": {
+             "text": "Error code 404",
+             "x": 0.05,
+             "xanchor": "left",
+         },
+         "xaxis": {"fixedrange": False},
+         "yxaxis": {"fixedrange": False},
+         "colorway": ["#e74c3c"],
+        },
+    }
+    figures.append(fig_error)
+
+    fig_clientConnections = {
+         "data": [
+             {
+                 "x": df_list[1]["Date"],
+                 "y": df_list[1]["OCCURRENCE"],
+                 "type": "bar",
+                 "hovertemplate": "%{y} %{x}"
+                                  "<extra></extra>",
+             },
+         ],
+         "layout": {
+             "title": {
+                 "text": "Client connections",
+                 "x": 0.05,
+                 "xanchor": "left",
+             },
+             "xaxis": {"fixedrange": False},
+             "yaxis": {"fixedrange": False},
+
+             "colorway": ["#16a085"],
+         },
+    }
+
+    figures.append(fig_clientConnections)
+
+    fig_requestUrl = {
+                 "data": [
+                     {
+                         "x": df_list[2]["URL"],
+                         "y": df_list[2]["OCCURRENCE"],
+                         "type": "bar",
+                         "hovertemplate": "%{y} %{x}"
+                                          "<extra></extra>",
+                     },
+                 ],
+                 "layout": {
+                     "title": {
+                         "text": "URL Request",
+                         "x": 0.05,
+                         "xanchor": "left",
+                     },
+                     "xaxis": {"fixedrange": False},
+                     "yaxis": {"fixedrange": False},
+
+                     "colorway": ["#f39c12"],
+                 },
+             }
+
+    figures.append(fig_requestUrl)
+
+    return figures;
+
+def update_uptime(uptime_server):
+    children = "Uptime : " + str(uptime_server)
+    return children
