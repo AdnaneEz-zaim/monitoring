47407bf1cea5a2388a8bf096f8a4fe8cd649ddf6
mariame.khanfri <mariame.khanfri@telecom-st-etienne.fr>
2023-01-13 10:46:46 +0100

    Error handler for ssh_exception.SSHException

--- models/Connexion.py
+++ models/Connexion.py
@@ -4,11 +4,18 @@ class Connexion :
     """Class that enable the connexion"""
 
     def __init__(self,monitor):
-        self.monitor=monitor
-        self.client = paramiko.SSHClient()
-        self.client.load_system_host_keys()
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-        self.client.connect(hostname=self.monitor[0], username=self.monitor[1], password=self.monitor[2], port=self.monitor[3])
+        
+        try:
+            self.monitor=monitor
+            self.client = paramiko.SSHClient()
+            self.client.load_system_host_keys()
+            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+            self.client.connect(hostname=self.monitor[0], username=self.monitor[1], password=self.monitor[2], port=self.monitor[3])
+        except (paramiko.ssh_exception.SSHException) as e:
+            print('ERROR', ('A connection to the server could not be established '))
+
+                
+        
     def get_client(self):
         """method to get client"""
         print(self.client)
