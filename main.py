05d642dd74ca563eb459f8e5d7f41e7bcd825940
axelantoine <ax.antoine42@gmail.com>
2023-01-18 21:31:38 +0100

    switch off debug mode

--- main.py
+++ main.py
@@ -248,4 +248,4 @@ def get_input_value(n_clicks, inputHostname, inputUsername, inputPassword, input
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)
